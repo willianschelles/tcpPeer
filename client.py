77f43e2e4e19c387a19a8ef969ea2cbe3bb207e5
igor <igorbpersona@gmail.com>
2017-06-05 22:52:46 -0300

    sem erro, vetor errado

--- client.py
+++ client.py
@@ -10,6 +10,12 @@ print peerId
 # number of peers
 N = int(sys.argv[2])
 
+# create list of active peers
+peerIdList = []
+for i in range(N) :
+        peerIdList.append(0)
+
+
 if peerId == 0:
     leader = 1
 else:
@@ -17,6 +23,7 @@ else:
 
 while True:
 	time.sleep(3)
+	port = 5000
 	i = 1
 	for i in range(N):
 	#    try:
@@ -25,28 +32,28 @@ while True:
 	    try:
 	        #print >>sys.stderr, '##########CLIENT'
 	        # Connect the socket to the port where the server is listening
-	        sock.connect(("localhost", port))
-	        port += 1
+	        sock.connect(("localhost", port + i))
 	        # Send data
 	        message = '%d' % peerId
 	        #message += leader
-	        print >>sys.stderr, 'Eviando msg: "%s" to port: %d' % (message, port)
+	        print >>sys.stderr, 'Eviando msg: "%s" to port: %d' % (message, port+i)
 	        sock.sendall(message)
 
 	        # while amount_received < amount_expected:
 	        data = sock.recv(64)
+
 	        # amount_received += len(data)
 	        #print >>sys.stderr, 'received "%s"' % data
 	    except socket.error, exc:
 	        print "except error: %s" % str(exc)
-	        print "caiu porta: %d" % str(port)
-	        print "except error: %s" % errorMessage
+	        print "caiu a porta: %d" % port+i
 	        
 	     #   if leader == port:
 
 	      #  sock.sendall(errorMessage)
 
 	    finally:
-	            print >>sys.stderr, 'finallyClient----------'
-	            #sock.close()
+	    	#port += 1
+	        print >>sys.stderr, 'finallyClient----------'
+	        sock.close()
 
