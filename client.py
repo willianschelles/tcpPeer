28d80d7a771f47653fd51493f4239643f5d1102f
igor <igorbpersona@gmail.com>
2017-06-05 22:10:59 -0300

    dormindo 3 segundos apos enviar msg

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import time
 
 port = 5000;
 
@@ -14,36 +15,38 @@ if peerId == 0:
 else:
     leader = 0
 
-i = 1
-for i in range(N):
-#    try:
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        print >>sys.stderr, '##########CLIENT'
-        # Connect the socket to the port where the server is listening
-        sock.connect(("localhost", port))
-        port += 1
-        # Send data
-        message = '%d' % peerId
-        #message += leader
-        print >>sys.stderr, 'sending "%s"' % message
-        sock.sendall(message)
+while True:
+	time.sleep(3)
+	i = 1
+	for i in range(N):
+	#    try:
+	    # Create a TCP/IP socket
+	    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    try:
+	        #print >>sys.stderr, '##########CLIENT'
+	        # Connect the socket to the port where the server is listening
+	        sock.connect(("localhost", port))
+	        port += 1
+	        # Send data
+	        message = '%d' % peerId
+	        #message += leader
+	        print >>sys.stderr, 'Eviando msg: "%s" to port: %d' % (message, port)
+	        sock.sendall(message)
 
-        # while amount_received < amount_expected:
-        data = sock.recv(64)
-        # amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
-    except socket.error, exc:
-        print "except error: %s" % exc
-        errorMessage = str(exc) + "caiu porta: %d" + str(port)
-        print "except error: %s" % errorMessage
-        
-     #   if leader == port:
+	        # while amount_received < amount_expected:
+	        data = sock.recv(64)
+	        # amount_received += len(data)
+	        #print >>sys.stderr, 'received "%s"' % data
+	    except socket.error, exc:
+	        print "except error: %s" % str(exc)
+	        print "caiu porta: %d" % str(port)
+	        print "except error: %s" % errorMessage
+	        
+	     #   if leader == port:
 
-      #  sock.sendall(errorMessage)
+	      #  sock.sendall(errorMessage)
 
-    finally:
-            print >>sys.stderr, 'closing socket'
-            sock.close()
+	    finally:
+	            print >>sys.stderr, 'finallyClient----------'
+	            #sock.close()
 
--- servidor.py
+++ servidor.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 
+
 print(sys.argv)
 
 # first argument is port to listen
@@ -9,7 +10,7 @@ print(sys.argv)
 
 # current peer ID
 peerId = int(sys.argv[1])
-print peerId
+#print peerId
 
 # number of peers
 N = int(sys.argv[2])
@@ -27,7 +28,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
 server_address = ('localhost', port)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+#print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -42,19 +43,19 @@ while True:
 
 
     # Wait for a connection
-    print >>sys.stderr, '##########SERVAR: waiting for a connection'
+    #print >>sys.stderr, '##########SERVAR: waiting for a connection'
     connection, client_address = sock.accept()
     try:
         print >>sys.stderr, 'connection from', client_address
 
         # Receive the data in small chunks and retransmit it
         while True:
-            peerIdRcvd = connection.recv(32) #data is peerId
-            print >>sys.stderr, 'peerIdRcvd "%s"' % peerIdRcvd
-            if peerIdRcvd:
-                teste = int(peerIdRcvd)
-                print >>sys.stderr, 'teste peer Id RCVD %d' % teste
-                peerIdList[teste] = 1 # confirm that the peer is
+            dataRcvd = connection.recv(32) #data is peerId
+            #print >>sys.stderr, 'peerIdRcvd "%s"' % peerIdRcvd
+            if dataRcvd:
+                peerIdRcvd = int(dataRcvd)
+                print >>sys.stderr, 'Recebido: %d' % peerIdRcvd
+                peerIdList[peerIdRcvd] = 1 # confirm that the peer is
 
                 print peerIdList
 
@@ -66,25 +67,26 @@ while True:
                 #     peerIdConcate += str(peerIdList[i])
                     
 
-                print >>sys.stderr, 'PeerIdCocate = ', peerIdConcate
+                #print >>sys.stderr, 'PeerIdCocate = ', peerIdConcate
 
                 #send active peers to client
-                print >>sys.stderr, 'sending data back to the client'
-                print >>sys.stderr, '%s' % peerIdConcate 
+                print >>sys.stderr, 'Enviando para o cliente a msg: "%s"' % peerIdConcate
+                #print >>sys.stderr, '%s' % peerIdConcate 
                 connection.sendall(peerIdConcate)
                 #connection.sendal
             else:
-                print >>sys.stderr, '########## no more data from', client_address
+                print >>sys.stderr, '-no data-', client_address
                 break
 
         # verify if this peer is the new leader
-        for j in range(N) :
-            if j == peerId:
-                lider = 1
-                print >>sys.stderr, 'eu ', client_address
-            if peerIdList[j] == 1:
-                # leader is peerIdList[j]
-                break
+        if leader == 0:
+	        for j in range(N) :
+	            if j == peerId:
+	                lider = 1
+	                print >>sys.stderr, 'eu sou o novo lider [peerId]: %d' % peerId
+	            if peerIdList[j] == 1:
+	                # leader is peerIdList[j]
+	                break
 
                 
             
